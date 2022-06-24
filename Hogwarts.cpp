00a1ed8ea3432a8954a25430ea1e9c3bbcdf0bda
Muhammad Dzikri <94848528+Dikito08@users.noreply.github.com>
2022-06-24 14:23:42 +0700

    Update Hogwarts.cpp

--- Hogwarts.cpp
+++ Hogwarts.cpp
@@ -2,447 +2,308 @@
 #include <conio.h>
 #include <time.h>
 #include <windows.h>
-#include <MMsystem.h>
 
 using namespace std;
 
-// Define ASCII Arrow Key
 #define KEY_UP 72
 #define KEY_DOWN 80
 #define KEY_LEFT 75
 #define KEY_RIGHT 77
-#define ESC 27
 
-// Define function Color Set
-void setcolor(unsigned short color)
+void setcolor (unsigned short color)
 {
-	HANDLE hCon = GetStdHandle(STD_OUTPUT_HANDLE);
-	SetConsoleTextAttribute(hCon, color);
+  HANDLE hCon = GetStdHandle (STD_OUTPUT_HANDLE);
+
+  SetConsoleTextAttribute (hCon, color);
+
 }
 
-// Define function to Move the Player
-void playerMovement(int key);
 
-// Define Function of coloring and symbolic
-void displayMap();
+struct posisi
+	{	
+    	int panjangPeta;
+    	int lebarPeta;
+    	int koordinatY;
+    	int koordinatX;
+    	int koordinat;
+  	};
 
-// Define function Color Set
-void setcolor(unsigned short color);
-
-const int panjangPeta = 20;
-const int lebarPeta = 20;
-int playerX = 7;
-int playerY = 7;
-int arrowKey = ' ';
-
-
-// Define Map
-int peta[lebarPeta][panjangPeta] = {
-		{9, 9, 9, 9, 9, 9, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 7, 7, 7, 7},
-		{9, 9, 9, 9, 9, 9, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 7},
-		{9, 9, 9, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 1, 1, 1, 1, 1, 1, 7},
-		{9, 9, 9, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 1, 1, 1, 1, 1, 1, 7},
-		{1, 9, 9, 9, 9, 9, 9, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-		{1, 1, 9, 9, 9, 9, 9, 1, 1, 1, 1, 1, 1, 7, 7, 7, 9, 1, 1, 1},
-		{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 7, 7, 7, 7, 7, 9, 1, 1, 1},
-		{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 7, 7, 7, 7, 7, 7, 9, 1, 1, 1},
-		{1, 1, 1, 8, 1, 1, 1, 1, 1, 1, 1, 1, 7, 7, 7, 7, 9, 8, 6, 8},
-		{1, 1, 8, 1, 9, 1, 1, 1, 7, 7, 7, 7, 7, 7, 7, 7, 9, 1, 1, 1},
-		{1, 8, 1, 1, 9, 1, 1, 1, 1, 1, 7, 7, 7, 7, 7, 7, 9, 1, 1, 1},
-		{8, 1, 4, 1, 1, 1, 1, 1, 9, 9, 9, 9, 9, 9, 9, 9, 9, 1, 1, 1},
-		{1, 8, 1, 1, 9, 1, 1, 1, 1, 1, 9, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-		{1, 1, 8, 1, 9, 1, 1, 1, 1, 1, 8, 1, 1, 6, 1, 1, 1, 2, 2, 1},
-		{1, 1, 1, 8, 1, 1, 1, 1, 1, 1, 8, 1, 1, 1, 1, 1, 1, 2, 2, 1},
-		{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 1},
-		{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-		{1, 1, 2, 2, 1, 1, 1, 1, 1, 1, 8, 1, 1, 1, 1, 1, 8, 8, 8, 8},
-		{1, 1, 2, 2, 1, 1, 1, 1, 1, 1, 8, 1, 1, 1, 1, 1, 1, 1, 5, 1},
-		{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 9, 1, 1, 1, 1, 1, 8, 1, 1, 1},
-};
-
-// Define Player Structure
-struct Hero
+int main ()
 {
-	string name;
-	int hp;
-	int atk;
-	int def;
+  // Game dojo suit
+  /*
+     1 jalan biasa : _
+     2 orang : !
+     4 tembok : =
+     5 kayu : -
+     6 Batu : o
+   */
 
-	void baseAtkPlayer(int *Pokemon_hp, int *Pokemon_def)
-	{
-		*Pokemon_hp = *Pokemon_hp - (atk - *Pokemon_def);
-	}
-};
+//    int panjangPeta = 20; // x
+//    int lebarPeta = 16; // y
 
-// Define Pokemon Structure
-struct Pokemon
-{
-	string name;
-	int hp;
-	int atk;
-	int def;
+//    int posisiKarakterY = 15;
+//    int posisiKarakterX = 4;
 
-	void baseAtkPokemon(int *player_hp, int *player_def)
-	{
-		*player_hp = *player_hp - (atk - *player_def);
-	}
-};
+	posisi myPosisi;
 
-//Define function to Move the Player
-void playerMovement(int key)
-{
-  	if (key == KEY_UP
-  	&& peta[playerY - 1][playerX] < 7)
-	{
-	  playerY -= 1;
-	  playerY < 0 ? playerY =
-	    0 : playerY;
-	}
-      else if (key == KEY_DOWN
-		&& peta[playerY + 1][playerX] < 7)
-	{
-	  playerY += 1;
-	  playerY > 19 ? playerY =
-	    19 : playerY;
-	}
-      else if (key == KEY_LEFT
-	       && peta[playerY][playerX - 1] < 7)
-	{
-	  playerX -= 1;
-	  playerX < 0 ? playerX =
-	    0 : playerX;
-	}
-      else if (key == KEY_RIGHT
-	       && peta[playerY][playerX + 1] < 7)
-	{
-	  playerX += 1;
-	  playerX > 19 ? playerX =
-	    19 : playerX;
-	}
-	  else if (key == ESC)
-    {  
-    	system("cls");
-    	char	 choice;
-    	cout << "Are you sure want to exit?" << endl;
-    	cout << "1. Yes \n2. No\n";
-    	choice = getch();
+	myPosisi.panjangPeta = 40;
+	myPosisi.lebarPeta = 24;
+	myPosisi.koordinatY = 8;
+	myPosisi.koordinatX = 0;
 
-    	if (choice == '1') {
-			exit(0);
-		} else if (choice == '2') {
-			system("cls");
-		}
-    };     
-}
 
-// Define Function of coloring and symbolic
-void displayMap()
-{
-	int koordinat = peta[playerY][playerX];
-	
-	cout << "===========================\n";
-	cout << "=   Welcome to Hogwarts   =\n";
-	cout << "===========================\n";
-	
-	for (int i = 0; i < lebarPeta; i++)
+	cout << "Karakter ada di y:" << myPosisi.
+	koordinatX << ", x:" << myPosisi.koordinatY << "\n";
+
+	int peta[myPosisi.lebarPeta][myPosisi.panjangPeta] = {
+    {4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4},
+    {4, 2, 1, 1, 1, 1, 5, 6, 5, 2, 1, 1, 1, 1, 1, 1, 1, 1, 2, 4},
+    {4, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 4},
+    {4, 1, 1, 1, 1, 1, 5, 1, 5, 5, 5, 5, 5, 1, 5, 5, 5, 5, 5, 4},
+    {4, 1, 1, 1, 1, 1, 5, 1, 5, 2, 1, 1, 6, 1, 6, 1, 1, 1, 2, 4},
+    {4, 1, 1, 1, 1, 1, 5, 1, 5, 1, 1, 1, 6, 1, 6, 1, 1, 1, 1, 4},
+    {4, 5, 5, 5, 5, 5, 5, 1, 5, 1, 1, 1, 6, 1, 6, 1, 1, 1, 1, 4},
+    {1, 1, 1, 1, 1, 1, 1, 1, 5, 1, 1, 1, 6, 1, 6, 1, 1, 1, 1, 4},
+    {1, 1, 1, 1, 1, 1, 1, 1, 5, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 4},
+    {4, 5, 5, 5, 5, 5, 5, 1, 5, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 4},
+    {4, 1, 1, 1, 1, 1, 5, 1, 5, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 4},
+    {4, 1, 1, 1, 1, 1, 5, 1, 5, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 4},
+    {4, 1, 1, 1, 1, 1, 5, 1, 5, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 4},
+    {4, 1, 1, 1, 1, 1, 5, 1, 5, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 4},
+    {4, 2, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 2, 4},
+    {4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4},
+	};
+
+
+	int arrowKey;
+	/*
+     #define KEY_UP 72
+     #define KEY_DOWN 80
+     #define KEY_LEFT 75
+     #define KEY_RIGHT 77
+   */
+  
+	int game = 1;
+	while (game <= 1)
+    {
+      // Input Keyboard
+      main :
+	  cout << "Masukan arrow key ";
+      arrowKey = (getch ());
+      cout << "Arrow key yang dimasukan " << myPosisi.koordinat << "\n";
+
+      // Aturan menggerakkan karakter ke atas
+      if (arrowKey == KEY_UP
+	  && peta[myPosisi.koordinatY - 1][myPosisi.koordinatX] < 4)
 	{
-	  for (int j = 0; j < panjangPeta; j++)
+	  myPosisi.koordinatY -= 1;
+	  myPosisi.koordinatY < 0 ? myPosisi.koordinatY =
+	    0 : myPosisi.koordinatY;
+	}
+      else if (arrowKey == KEY_DOWN
+	       && peta[myPosisi.koordinatY + 1][myPosisi.koordinatX] < 4)
+	{
+	  myPosisi.koordinatY += 1;
+	  myPosisi.koordinatY > 15 ? myPosisi.koordinatY =
+	    15 : myPosisi.koordinatY;
+	}
+      else if (arrowKey == KEY_LEFT
+	       && peta[myPosisi.koordinatY][myPosisi.koordinatX - 1] < 4)
+	{
+	  myPosisi.koordinatX -= 1;
+	  myPosisi.koordinatX < 0 ? myPosisi.koordinatX =
+	    0 : myPosisi.koordinatX;
+	}
+      else if (arrowKey == KEY_RIGHT
+	       && peta[myPosisi.koordinatY][myPosisi.koordinatX + 1] < 4)
+	{
+	  myPosisi.koordinatX += 1;
+	  myPosisi.koordinatX > 19 ? myPosisi.koordinatX =
+	    19 : myPosisi.koordinatX;
+	}
+      else
+	{
+	  // nothing to do
+	};
+
+      myPosisi.koordinat = peta[myPosisi.koordinatY][myPosisi.koordinatX];
+      system ("cls");
+      for (int i = 0; i < myPosisi.lebarPeta; i++)
+	{
+	  for (int j = 0; j < myPosisi.panjangPeta; j++)
 	    {
-	      	if (playerX == j && playerY == i
-		  	&& koordinat < 7)
-			{
-		  		setcolor (13);		//pink
-		  		cout << "&";		//Player
-		  		setcolor (7);
-			}
-	      		else if (peta[i][j] == 1)
-			{
-		  		setcolor (14);		//Yellow
-		  		cout << "_";		//Rumput pendek
-		  		setcolor (7);
-			}
-	      		else if (peta[i][j] == 2)
-			{
-		  		setcolor (9);		//Blue
-		  		cout << "@";		//Genangan air
-		  		setcolor (7);
-			}
-	      		else if (peta[i][j] == 3)
-			{
-		  		setcolor (13);		//pink
-		  		cout << "&";		//Harry Potter
-		  		setcolor (7);
-			}
-	      		else if (peta[i][j] == 4)
-			{
-		  		setcolor (4);		//Maroon
-		  		cout << "M";		//Acromantula
-		  		setcolor (7);
-			}
-	      		else if (peta[i][j] == 5)
-			{
-		  		setcolor (12);		//Red
-		  		cout << "Z";		//Basilisk
-		  		setcolor (7);
-			}
-		  		else if (peta[i][j] == 6)
-			{
-		  		setcolor (11);		//cyan
-		  		cout << "G";		//Dementors
-		 		 setcolor (7);
-			}
-		  		else if (peta[i][j] == 7)
-			{
-		  		setcolor (2);		//Green
-		  		cout << "^";		//Pohon
-		 		setcolor (7);
-			}	
-	      		else if (peta[i][j] == 8)
-			{
-		  		setcolor (8);		//Grey
-		  		cout << "o";		//Batu
-		  		setcolor (7);
-			}
-	     		else if (peta[i][j] == 9)
-			{
-		  		setcolor (10);		//Light Green
-		  		cout << "#";		//Semak
-		  		setcolor (7);
-			}
-	     		else
-	     	{
-		  // nothing to do
+	      if (myPosisi.koordinatX == j && myPosisi.koordinatY == i
+		  && myPosisi.koordinat < 4)
+		{
+		  setcolor (13);
+		  cout << "$";
+		  setcolor (7);
+		}
+	      else if (peta[i][j] == 1)
+		{
+		  setcolor (11);
+		  cout << "_";
+		  setcolor (7);
+		}
+	      else if (peta[i][j] == 2)
+		{
+		  setcolor (4);
+		  cout << "!";
+		  setcolor (7);
+		}
+	      else if (peta[i][j] == 4)
+		{
+		  setcolor (2);
+		  cout << "=";
+		  setcolor (7);
+		}
+	      else if (peta[i][j] == 5)
+		{
+		  setcolor (1);
+		  cout << "-";
+		  setcolor (7);
+		}
+	      else if (peta[i][j] == 6)
+		{
+		  setcolor (8);
+		  cout << "o";
+		  setcolor (7);
 		}
 	    }
 	  cout << "\n";
-	};
-}
 
-int main()
-{
-		
-	while (true)
+	};
+
+
+      // Ketika bertemu lawan   
+      if (myPosisi.koordinat == 2 )
 	{
-		// Gerak Player
-		playerMovement(arrowKey);
-		
-		// Tampilan Peta
-		displayMap();
-		
-		// Input Keyboard
-		cout << "CLick Arrow Key to Play\n";
-		cout << "-----------------------\n";
-		cout << "   Click ESC to exit   \n";
-		
-		arrowKey = getch();
-		
-		//Clear Screen
-		system("cls");
+	      int choice;
 
-		// Hero Object
-		Hero player;
-		player.name = "Harry Potter";
-		player.hp = 500;
-		player.atk = 50;
-		player.def = 10;
+	      cout << "\n\nApakah kau yakin ingin melawannya?\n";
+	      cout << "1. Ya\n2. Tidak\n";
+	      cout << "Pilihan : ";
+	      cin >> choice;
 
-		// find pokemon
-		if (peta[playerY][playerX] == 4)
+		//system("clear");
+	      if (choice == 1)
 		{
-			// Pokemon Object
-			Pokemon pokemon1;
-			pokemon1.name = "Acromantula";
-			pokemon1.hp = 300;
-			pokemon1.atk = 25;
-			pokemon1.def = 15;
-
-			while (true)
-			{
-				system("cls");
-
-				char input_battle = ' ';
-				cout << "You found " << pokemon1.name << " Harrry!!" << endl;
-				cout << player.name << ": " << player.hp << endl;
-				cout << pokemon1.name << ": " << pokemon1.hp << endl;
-				cout << "Click the Q button to attack!" << endl;
-				input_battle = getch();
-
-				if (input_battle == 'q' || input_battle == 'Q')
-				{
-					player.baseAtkPlayer(&pokemon1.hp, &pokemon1.def);
-					pokemon1.baseAtkPokemon(&player.hp, &player.def);
-				}
-
-				if (pokemon1.hp <= 0)
-				{
-					peta[playerY][playerX] = 1;
-					setcolor(11);
-					cout << "Harry has slain " << pokemon1.name << endl;
-					setcolor(7);
-					
-					setcolor(12);
-					cout << "Wait 4 sekon" << endl;
-					setcolor(7);
-					
-					Sleep(4000);
-					system("cls");
-					break;
-				}
-			}
-		}
-		if (peta[playerY][playerX] == 5)
-		{
-			// Pokemon Object
-			Pokemon pokemon2;
-			pokemon2.name = "Basilisk";
-			pokemon2.hp = 200;
-			pokemon2.atk = 25;
-			pokemon2.def = 10;
-
-			while (true)
-			{
-				system("cls");
-
-				char input_battle = ' ';
-				cout << "You found " << pokemon2.name << " Harrry!!" << endl;
-				cout << player.name << ": " << player.hp << endl;
-				cout << pokemon2.name << ": " << pokemon2.hp << endl;
-				cout << "Click the Q button to attack!" << endl;
-				input_battle = getch();
-
-				if (input_battle == 'q' || input_battle == 'Q')
-				{
-					player.baseAtkPlayer(&pokemon2.hp, &pokemon2.def);
-					pokemon2.baseAtkPokemon(&player.hp, &player.def);
-				}
-
-				if (pokemon2.hp <= 0)
-				{
-					peta[playerY][playerX] = 1;
-					setcolor(11);
-					cout << "Harry has slain " << pokemon2.name << endl;
-					setcolor(7);
-						
-					setcolor(12);
-					cout << "Wait 4 sekon" << endl;
-					setcolor(7);
-					
-					Sleep(4000);
-					system("cls");
-					break;
-				}
-			}
-		}
-		if (peta[playerY][playerX] == 6)
-		{
-			// Pokemon Object
-			Pokemon pokemon3;
-			pokemon3.name = "Dementors";
-			pokemon3.hp = 100;
-			pokemon3.atk = 25;
-			pokemon3.def = 5;
-
-			while (true)
-			{
-				system("cls");
-
-				char input_battle = ' ';
-				cout << "You found " << pokemon3.name << " Harrry!!" << endl;
-				cout << player.name << ": " << player.hp << endl;
-				cout << pokemon3.name << ": " << pokemon3.hp << endl;
-				cout << "Click the Q button to attack!" << endl;
-				input_battle = getch();
-
-				if (input_battle == 'q' || input_battle == 'Q')
-				{
-					player.baseAtkPlayer(&pokemon3.hp, &pokemon3.def);
-					pokemon3.baseAtkPokemon(&player.hp, &player.def);
-				}
-
-				if (pokemon3.hp <= 0)
-				{
-					peta[playerY][playerX] = 1;
-					setcolor(11);
-					cout << "Harry has slain " << pokemon3.name << endl;
-					setcolor(7);
-					
-					setcolor(12);
-					cout << "Wait 4 sekon" << endl;
-					setcolor(7);
-					
-					Sleep(4000);
-					system("cls");
-					break;
-				}
-			}
-		}
-		else if (peta[playerY][playerX] == 2)
-		{
-			int chance;
+			system ("cls");
+		  int pilih, lagi;
+		a:
+		cout << "\n\nSelamat datang di game suit" << endl;
+		cout << "Kamu pilih?\n\n";
+		cout << "1. Gajah\n2. Semut\n3. Orang\n\n";
+		cout << "Silahkan Pilih : ";
+		cin >> pilih;
+		cout << endl;
+		int com;
+		
+		if (pilih == 1) {
+			cout << "Kamu memilih gajah\n";
 			srand(time(NULL));
-			chance = rand() % 10;
-
-			if (chance < 3)
+			com = rand() % 10;
+			if (com <= 3 && com >= 1)
 			{
-				while(true)
-				{
-					system("cls");
-					int choice;
-					cout << "You Found Enemy, Harry!"<<endl;
-	      			cout << "1. Fight\n2. Run" << endl;
-	      			cout << "What will you do?";
-					choice = getch();
-				
-	      			if (choice == '1')
-					{
-	      				//Pokemon Object
-						Pokemon pokemon4;
-						pokemon4.name = "Bowtruckle";
-						pokemon4.hp = 100;
-						pokemon4.atk = 25;
-						pokemon4.def = 5;
-						
-						while(true)
-						{	
-							system("cls");
-							char input_battle = ' ';
-							cout << "You found " << pokemon4.name << " Harrry!!" << endl;
-							cout << player.name << ": " << player.hp << endl;
-							cout << pokemon4.name << ": " << pokemon4.hp << endl;
-							cout << "Click the Q button to attack!" << endl;
-							input_battle = getch();
-							
-							if (input_battle == 'q' || input_battle == 'Q')
-							{
-								player.baseAtkPlayer(&pokemon4.hp, &pokemon4.def);
-								pokemon4.baseAtkPokemon(&player.hp, &player.def);
-							}
-							if (pokemon4.hp <= 0)
-							{
-								setcolor(11);
-								cout << "Harry has slain " << pokemon4.name << endl;
-								setcolor(7);
-								
-								setcolor(12);
-								cout << "Wait 4 sekon" << endl;
-								setcolor(7);
-								
-								Sleep(4000);
-								system("cls");
-								break;
-							}
-						}
-						break;
-					}
-					else if (choice == '2')
-					{
-						break;
-					}
-				
-				}
+				cout << "Computer = Gajah\n\n";
+				cout << "Seri\n";
+			} else if (com <=6 && com >=4)
+			{
+				cout << "Computer = Orang\n\n";
+				cout << "Kamu menang\n";
+			} else 
+			{
+				cout << "Computer = Semut\n\n";
+				cout << "Kamu kalah\n";
+			} cout << endl;
+		}
+		else if (pilih == 2){
+			cout << "Kamu memilih semut\n";
+			srand(time(NULL));
+			int com = rand() % 10;
+			if (com <= 3 && com >= 1)
+			{
+				cout << "Computer = Semut\n\n";
+				cout << "Seri\n"; 
+			} else if (com <=6 && com >=4)
+			{
+				cout << "Computer = Gajah\n\n";
+				cout << "Kamu menang\n";
+			} else 
+			{
+				cout << "Computer = Orang\n\n";
+				cout << "Kamu kalah\n";
+			} cout << endl;
+		}
+		else if (pilih == 3) {
+			cout << "Kamu memilih orang\n";
+			srand(time(NULL));
+			int com = rand() % 10;
+			if (com <= 3 && com >= 1)
+			{
+				cout << "Computer = Orang\n\n";
+				cout << "Seri\n"; 
+			} else if (com <=6 && com >=4)
+			{
+				cout << "Computer = Semut\n\n";
+				cout << "Kamu menang\n";
+			} else 
+			{
+				cout << "Computer = Gajah\n\n";
+				cout << "Kamu kalah\n";
+			} cout << endl;
+		} else {
+			cout << "Nomor yang anda input salah" << endl;
+		}
+
+		if (com <=3 && com >= 1)
+		{
+			// Ketika seri
+			cout << "Pilih lagi??\n";
+			cout << "1. lagi\n2. tidak\n";
+			cout << "pilihan : ";
+			cin >> lagi;
+		b:
+			if (lagi == 1){
+				system ("cls");
+				goto a;
+		}
+		  getch ();
+		} else if (com <= 6 && com >= 4) 
+		{
+			//Ketika menang, jika memilih tidak maka akan keluar dari game
+			int lanjut;
+			cout << "lanjut main??" << endl;
+			cout << "1. lanjut\n2. tidak\n";
+			cout << "pilihan : ";
+			cin >> lanjut;
+			if (lanjut == 1) 
+			{
+				system ("cls");
+				goto main;
+			} else if (lanjut == 2)
+			{
+				game++;
+			}
+		} else
+		{
+			//Ketika kalah
+			int coba;
+			cout << "Coba lagi ??\n";
+			cout << "1. Ya\n2. Tidak\n";
+			cout << "pilihan";
+			cin >> coba;
+			if (coba == 1)
+			{
+				system ("cls");
+				goto a;
 			}
 		}
-	};
-	getch();
-	return 0;
-}
+	}
+    };
+	}
+	system ("cls");
+	cout << "Terima Kasih Telah Bermain" << endl;
+  return 0;
+};
